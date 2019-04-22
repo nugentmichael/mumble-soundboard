79e62c40228e2c3c3aeb11fdb3be30d98527509e
nugentmichael <nugent.michael@gmail.com>
2019-04-22 15:55:49 -0400

    Add new file upload functionality.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,8 +1,29 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+    const url = 'index.php';
+    const audioForm = document.getElementById('audio-upload');
+
+    audioForm.addEventListener('submit', event => {
+        event.preventDefault();
+
+        const files = document.querySelector('[type=file]').files;
+        const formData = new FormData();
+
+        for (let i = 0; i < files.length; i++) {
+            formData.append('files[]', file);
+        }
+
+        fetch(url, {
+            method: 'POST',
+            body: formData,
+        }).then(response => {
+            console.log(response);
+        });
+    });
+
     const clips = document.querySelectorAll('.audio-file');
 
     clips.forEach(function(clip) {
-        clip.addEventListener('click', function(event) {
+        clip.addEventListener('click', event => {
             event.preventDefault();
 
             const audio = new Audio(this.href);
